be1f069c88339d8b442dbd5d57175ac9191f06b2
Abdul <abdul.a@diamondxe.com>
2025-05-23 11:41:42 +0530

    hanges done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -43,9 +43,9 @@ const HomePage: React.FC = () => {
 
               <div className="flex items-center mt-10">
                 <div className="flex -space-x-3">
-                  <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Client" className="w-10 h-10 rounded-full border-2 border-gray-900" />
-                  <img src="https://images.pexels.com/photos/3775534/pexels-photo-3775534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Client" className="w-10 h-10 rounded-full border-2 border-gray-900" />
-                  <img src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Client" className="w-10 h-10 rounded-full border-2 border-gray-900" />
+                  <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Client" className="w-10 h-10 rounded-full border-2 border-gray-900" />
+                  <img src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Client" className="w-10 h-10 rounded-full border-2 border-gray-900" />
+                  <img src="https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Client" className="w-10 h-10 rounded-full border-2 border-gray-900" />
                 </div>
                 <div className="ml-4">
                   <div className="flex items-center mb-1">
